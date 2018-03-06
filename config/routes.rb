bb2405db45691846a45b7d30ae2687b363f6762f
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-06 14:14:58 +0100

    improve code

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
+  # get '/tasks', to: 'tasks#index'
 
-  post '/tasks', to: 'tasks#create'
+  # post '/tasks', to: 'tasks#create'
 
-  get '/tasks/new', to: 'tasks#new'
+  # get '/tasks/new', to: 'tasks#new'
 
-  get '/tasks/:id/edit', to: 'tasks#edit', as: "tasks_edit"
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: "tasks_edit"
 
-  get '/tasks/:id', to: 'tasks#show', as: "task"
+  # get '/tasks/:id', to: 'tasks#show', as: "task"
 
-  patch '/tasks/:id', to: 'tasks#update'
+  # patch '/tasks/:id', to: 'tasks#update'
 
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
